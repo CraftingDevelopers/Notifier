33b0383ef4647741df72461f05d89d90e20c2be8
Jan Kirby <coolguy412@mailinator.com>
2016-04-17 19:59:38 +0800

    Update Main.php

--- src/Jankirby/Notifier/Main.php
+++ src/Jankirby/Notifier/Main.php
@@ -10,25 +10,24 @@ use pocketmine\command\CommandSender;
 
 class Main extends PluginBase implements Listener{
 
-public function isWhitelisted(){
+public function isWhitelisted(isWhitelisted $event){
   $sender->getServer()->broadcastMessage(TextFormat::RED."$name tried to join, but he/she is not on the whitelist-list!");
 }
 public function isBanned(){
   $sender->getServer()->broadcastMessage(TextFormat::RED."$name tried to join, but he/she is banned!");
 }
-  //public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-    //if($cmd->getName() == "checkop"){
-  //if(!isset($args[0])){
-  //$sender->sendMessage("Usage: /checkop <player>");
-  //}else{
-  //$player = $this->getServer()->getPlayer($args[0]);
-  //if($player instanceof Player){
-  //$sender->sendMessage("".$sender->getName()." says Hi to You!");
-  //if($player isOp )
-  //}else{
-  //$sender->sendMessage("Player Not Found!");
- //return $this->server->isOp($this->getName());
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    if($cmd->getName() == "checkop"){
+  if(!isset($args[0])){
+  $sender->sendMessage("Usage: /checkop <player>");
+  }else{
+  $player = $this->getServer()->getPlayer($args[0]);
+  if($player isOp){
+  $sender->sendMessage("".$sender->getName()."$player has operator status!");
+  if(!isset($args[0])){
+  $sender->sendMessage("$player do not have operator status");
 }
+
   
   
 
