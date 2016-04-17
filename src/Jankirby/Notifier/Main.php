0d98ae4623fbae2112eb0c7be70e8b71689bda14
Jan Kirby <coolguy412@mailinator.com>
2016-04-17 20:05:08 +0800

    Update Main.php

--- src/Jankirby/Notifier/Main.php
+++ src/Jankirby/Notifier/Main.php
@@ -17,15 +17,10 @@ public function isBanned(){
   $sender->getServer()->broadcastMessage(TextFormat::RED."$name tried to join, but he/she is banned!");
 }
   public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-    if($cmd->getName() == "checkop"){
-  if(!isset($args[0])){
-  $sender->sendMessage("Usage: /checkop <player>");
-  }else{
-  $player = $this->getServer()->getPlayer($args[0]);
-  if($player isOp){
-  $sender->sendMessage("".$sender->getName()."$player has operator status!");
-  if(!isset($args[0])){
-  $sender->sendMessage("$player do not have operator status");
+    if($cmd->getName() == "notifier"){
+  $sender->sendMessage("".$sender->getName()."$sender This server uses Notifier by Jankirby");
+      }
+   }
 }
 
   
