a85d99a4cb14f7fc8730e2972615e455cb9dadbd
Jan Kirby <coolguy412@mailinator.com>
2016-04-18 20:59:16 +0800

    Update Main.php

--- src/Jankirby/Notifier/Main.php
+++ src/Jankirby/Notifier/Main.php
@@ -3,30 +3,30 @@
 namespace Jankirby\Notifier;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\Player;
+use pocketmine\event\player\PlayerPreLoginEvent;
 use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\player;
 
 class Main extends PluginBase implements Listener{
 
-public function isWhitelisted(isWhitelisted $event){
-  $sender->getServer()->broadcastMessage(TextFormat::RED."$name tried to join, but he/she is not on the whitelist-list!");
-}
-public function isBanned(){
-  $sender->getServer()->broadcastMessage(TextFormat::RED."$name tried to join, but he/she is banned!");
-}
-  public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
-		if($sender instanceof Player) {
-			$player = $sender->getPlayer()->getName();
-			if(strtolower($command->getName('Notifier'))) {
-				if(empty($args)) {
-					$sender->sendMessage($this->plugin->formatMessage(TextFormat::GOLD."This server uses Notifier by Jankirby"));
-					return true;
-				}
-			}
-		}
-  }
+ public function onPreLogin(PlayerPreLoginEvent $event){
+    $player = $event->getPlayer();
+    $reason = $player joined the server, but he/she is not on the whitelist list!
+    if(!$this->getServer()->isWhitelisted($player->getName())){
+      $event->setCancelled(true);
+      $event->setKickMessage($reason);
+        }
+    }
+     public function onPreLogin(PlayerPreLoginEvent $event){
+    $player = $event->getPlayer();
+    $reason = $player tried to join the game but he/she is banned!
+    if(!$this->getServer()->isBanned($player->getName())){
+      $event->setCancelled(true);
+      $event->setKickMessage($reason);
+    }
+     }
 }
 
 
