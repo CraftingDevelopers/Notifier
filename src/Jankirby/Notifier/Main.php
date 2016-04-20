883d5dd4986378ab432769175093b5b9f30dca25
Jan Kirby <coolguy412@mailinator.com>
2016-04-20 11:31:12 +0800

    Update Main.php

--- src/Jankirby/Notifier/Main.php
+++ src/Jankirby/Notifier/Main.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Jankirby\Notifier;
 
 use pocketmine\plugin\PluginBase;
@@ -7,29 +6,24 @@ use pocketmine\event\player\PlayerPreLoginEvent;
 use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\player;
+use pocketmine\Player;
 
 class Main extends PluginBase implements Listener{
 
- public function onPreLogin(PlayerPreLoginEvent $event){
+public function onPreLogin(PlayerPreLoginEvent $event){
     $player = $event->getPlayer();
-    $reason = $player joined the server, but he/she is not on the whitelist list!
+    $name = $player->getName();
     if(!$this->getServer()->isWhitelisted($player->getName())){
       $event->setCancelled(true);
-      $event->setKickMessage($reason);
-        }
+      $event->setKickMessage(C::RED . $name . "joined the server, but he/she is not on the whitelist!");
+       if(!$this->getServer()->isBanned($player->getName())){
+        $event->setCancelled(true);
+      $event->setKickMessage(C::RED . $name . "joined the server, but he/she is Banned!");
+      if(!$this->getServer()->isOp($player->getName())){
+        $event->setKickMessage(C::RED . $name . "is an OPERATOR, and is here to help!");
+       public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+      if($cmd->getName() == "test"){ $sender->sendMessage("This Is A Test!"); } return true; } }
     }
-     public function onPreLogin(PlayerPreLoginEvent $event){
-    $player = $event->getPlayer();
-    $reason = $player tried to join the game but he/she is banned!
-    if(!$this->getServer()->isBanned($player->getName())){
-      $event->setCancelled(true);
-      $event->setKickMessage($reason);
-    }
-     }
 }
-
-
-  
-  
-
+}
+}
